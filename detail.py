1dc3e8a911cd05abb91d0679991f80a91728ba5f
basavarajandanshetti-glitch <basavarajandanshetti@gmail.com>
2025-11-20 17:13:27 +0530

    Improve print statements for user input clarity

--- detail.py
+++ detail.py
@@ -1,16 +1,17 @@
 import sys
+
 if len(sys.argv) == 3:
     script_name = sys.argv[0]
     name = sys.argv[1]
     rollno = sys.argv[2]
-    print("user provided input values:")
-
+    print("User provided input values:")
 else:
     script_name = sys.argv[0]
     name = "raj"
     rollno = "123"
-    print(" no input given  default values:")
-    print("Student Name:", script_name)
-    print("Student Roll No:", rollno)
-    
+    print("No input given — using default values:")
+
+print("Student Name:", name)
+print("Student Roll No:", rollno)
+
     
